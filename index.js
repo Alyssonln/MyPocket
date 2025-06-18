2b512c46890bca555d258f7df5fd22ab3376c5eb
Alyssonln <alyssonlnunes@gmail.com>
2025-06-18 12:25:22 -0300

    Criação de funções auxiliares para criação dos elementos do DOM no arquivo index.js

---
+++ index.js
@@ -0,0 +1,32 @@
+function createTransactionContainer(id) {
+  const container = document.createElement('div')
+  container.classList.add('transaction')
+  container.id = `transaction-${id}`
+  return container
+}
+
+function createTransactionTitle(name) {
+  const title = document.createElement('span')
+  title.classList.add('transaction-title')
+  title.textContent = name
+  return title
+}
+
+function createTransactionAmount(amount) {
+  const span = document.createElement('span')
+  span.classList.add('transaction-amount')
+  const formater = Intl.NumberFormat('pt-BR', {
+    compactDisplay: 'long',
+    currency: 'BRL',
+    style: 'currency',
+  })
+  const formatedAmount = formater.format(amount)
+  if (amount > 0) {
+    span.textContent = `${formatedAmount} C`
+    span.classList.add('credit')
+  } else {
+    span.textContent = `${formatedAmount} D`
+    span.classList.add('debit')
+  }
+  return span
+}
